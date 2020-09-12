a19eb508a15ad7f47ae26f9a30bedfe99feea97e
mansong1 <martin.ansong@gmail.com>
2020-09-12 18:52:01 +0100

    Add loadbalancer resources

--- C1 - Azure Infrastructure Operations/project/starter_files/main.tf
+++ C1 - Azure Infrastructure Operations/project/starter_files/main.tf
@@ -44,3 +44,36 @@ resource "azurerm_network_interface" "main" {
   }
 }
 
+resource "azurerm_lb" "main" {
+  name                = "${var.prefix}-lb"
+  location            = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+
+  frontend_ip_configuration {
+    name                 = "PublicIPAddress"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+resource "azurerm_lb_backend_address_pool" "main" {
+  resource_group_name = azurerm_resource_group.main.name
+  loadbalancer_id     = azurerm_lb.main.id
+  name                = "BackEndAddressPool"
+}
+
+resource "azurerm_lb_nat_rule" "main" {
+  resource_group_name            = azurerm_resource_group.main.name
+  loadbalancer_id                = azurerm_lb.main.id
+  name                           = "HTTPSAccess"
+  protocol                       = "Tcp"
+  frontend_port                  = 443
+  backend_port                   = 443
+  frontend_ip_configuration_name = azurerm_lb.main.frontend_ip_configuration[0].name
+}
+
+resource "azurerm_network_interface_backend_address_pool_association" "main" {
+  count                   = local.instance_count
+  backend_address_pool_id = azurerm_lb_backend_address_pool.main.id
+  ip_configuration_name   = "primary"
+  network_interface_id    = element(azurerm_network_interface.main.*.id, count.index)
+}
